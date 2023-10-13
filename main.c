71d539d2469467a2db9d064b75977715a0f1ec0c
Louis Sabatier <lsabatie@z1r11p3.42lyon.fr>
2023-10-13 16:52:18 +0200

    first ocmmit, git init

---
+++ main.c
@@ -0,0 +1,17 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsabatie <lsabatie@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 10:32:43 by lsabatie          #+#    #+#             */
+/*   Updated: 2023/10/03 10:37:17 by lsabatie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int main(int ac, char **av)
+{
+    // ./pipex file1 cmd1 cmd2 file2
+    // file1 input de cmd1, output de cmd1 est input de cmd2, output de cmd2 va dans file2
+}
