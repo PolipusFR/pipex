a5c8895b47986907524be606db368d3498b533e6
Louis Sabatier <lsabatie@z4r12p1.42lyon.fr>
2024-02-02 09:36:58 +0100

    fixed text problem, ready to correct

--- src/ft_putstr_fd.c
+++ src/ft_putstr_fd.c
@@ -6,16 +6,11 @@
 /*   By: lsabatie <lsabatie@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 19:54:46 by lsabatie          #+#    #+#             */
-/*   Updated: 2024/02/02 09:24:44 by lsabatie         ###   ########.fr       */
+/*   Updated: 2024/02/02 09:35:20 by lsabatie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../incs/pipex.h"
-
-static void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -24,7 +19,7 @@ void	ft_putstr_fd(char *s, int fd)
 	i = 0;
 	while (s[i])
 	{
-		ft_putchar_fd(s[i], fd);
+		write(fd, &s[i], 1);
 		i++;
 	}
 }
